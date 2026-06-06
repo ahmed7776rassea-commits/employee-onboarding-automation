9acd2a7e4169b83677262e45a5954508f38784f1
Ahmed Rassea <ahmed.rassea@student.htw-berlin.de>
2026-06-06 23:27:38 +0200

    Read employee data from CSV

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using EmployeeOnboarding.Models;
+
+string dateiPfad = "data/employees.csv";
+List<Mitarbeiter> mitarbeiterListe = new List<Mitarbeiter>();
+
+if (File.Exists(dateiPfad))
+{
+    string[] zeilen = File.ReadAllLines(dateiPfad);
+
+    foreach (string zeile in zeilen.Skip(1))
+    {
+        string[] teile = zeile.Split(',');
+        
+        Mitarbeiter mitarbeiter = new Mitarbeiter();
+
+        mitarbeiter.MitarbeiterID = int.Parse(teile[0]);
+        mitarbeiter.Vorname = teile[1];
+        mitarbeiter.Nachname = teile[2];
+        mitarbeiter.Abteilung = teile[3];
+        mitarbeiter.Email = teile[4];
+        mitarbeiterListe.Add(mitarbeiter);
+
+        Console.WriteLine($"{mitarbeiter.MitarbeiterID} - {mitarbeiter.Vorname} {mitarbeiter.Nachname} - {mitarbeiter.Abteilung}");
+    }
+}
+else
+{
+    Console.WriteLine("Datei nicht gefunden!");
+}
