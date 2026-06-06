d30df7620f956c2ccd4dffaa3a642a3c14e581e8
Ahmed Rassea <ahmed.rassea@student.htw-berlin.de>
2026-06-06 23:24:12 +0200

    Create Mitarbeiter model

---
+++ Models/Mitarbeiter.cs
@@ -0,0 +1,11 @@
+namespace EmployeeOnboarding.Models;
+
+public class Mitarbeiter
+{
+    public int MitarbeiterID{get;set;}
+    public string Vorname{get;set;}
+    public string Nachname{get;set;}
+    public string Abteilung {get;set;}
+    public string Email{get;set;}
+
+}
